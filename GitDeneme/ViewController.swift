8e8165a36bd751ec73da2817489120415f61dec9
gokhanCengiz <37997473+gokhanCengiz@users.noreply.github.com>
2019-01-13 02:37:33 +0300

    Add files via upload

---
+++ GitDeneme/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitDeneme
+//
+//  Created by Gokhan Cengiz on 13.01.2019.
+//  Copyright © 2019 Gokhan Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Deneme")
+    }
+
+
+}
+
